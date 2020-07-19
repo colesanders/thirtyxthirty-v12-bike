43cbc6dc1a3ab0d23f31c77993122a7aebfc652b
Steve Witman <stevewitman@gmail.com>
2020-07-19 13:33:16 -0600

    initial commit

---
+++ apps/bike/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'bike',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/bike',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+};
